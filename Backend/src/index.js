04cd7eb25c40c15e7bc40e32ad25c853207ec5cb
Unshakhan <unshak08@gmail.com>
2026-05-15 21:18:42 +0500

    reupdate again

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,32 +1,31 @@
-import express from "express"
 import dotenv from "dotenv"
+dotenv.config()  // 👈 sabse pehle — upar le aao
+
+import express from "express"
 import connectDb from "./config/connect-DB.js"
 import cookieParser from "cookie-parser"
 import cors from "cors"
 import authRoute from "./routes/authRoutes.js"
 import blogRoute from "./routes/blogRoutes.js"
 
-const app= express()
-dotenv.config()
+const app = express()
 connectDb()
+
 app.use(cors({
-  origin:['http://localhost:5173','https://full-stack-blog-app-evn4.vercel.app'],
-  credentials:true
+  origin: ['http://localhost:5173', 'https://full-stack-blog-app-evn4.vercel.app'],
+  credentials: true
 }))
 app.use(express.json())
 app.use(cookieParser())
 
-app.get("/", (req,res)=>{
- res.send("helooo")
+app.get("/", (req, res) => {
+  res.send("helooo")
 })
 
-app.use("/api/v1/auth",authRoute)
-app.use("/api/v1/blog",blogRoute )
+app.use("/api/v1/auth", authRoute)
+app.use("/api/v1/blog", blogRoute)
 
-const PORT = process.env.PORT
-console.log("PORT---------->",PORT)
-
-app.listen(PORT, ()=>{
-  console.log(`Server is running on ${PORT}`
-  );
-  })
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`)
+})
